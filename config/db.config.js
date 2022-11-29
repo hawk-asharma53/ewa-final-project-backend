82160dd339951ab3d9441546c4a62035bce1c96e
biz-cloud-abhishek <103125284+biz-cloud-abhishek@users.noreply.github.com>
2022-11-29 09:59:35 -0600

    db changed

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ var config = {
     host     : '34.66.75.54',
     user     : 'root',
     password : '^d%Qb8w^v',
-    database : 'production'
+    database : 'development'
 }
 
 export default config
