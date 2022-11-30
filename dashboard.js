ff76a8902919590bb9cd770191d911c9f9088479
biz-cloud-abhishek <103125284+biz-cloud-abhishek@users.noreply.github.com>
2022-11-29 23:55:57 -0600

    dashboard done

--- dashboard.js
+++ dashboard.js
@@ -6,6 +6,7 @@ import productService from './services/product.service.js';
 
 const BASE_URL = '/api';
 const DASHBOARD = BASE_URL + '/dashboard';
+const DASHBOARD_BY_STORE = BASE_URL + '/dashboard/:storeId';
 
 export default function (app) {
   app.get(DASHBOARD, async (request, response) => {
@@ -23,6 +24,55 @@ export default function (app) {
   
       var thisWeekRevenue = await orderService.getTotalRevenueForDate(weekStart.format('YYYY-MM-DD'), weekEnd.format('YYYY-MM-DD'));
       var lastWeekRevenue = await orderService.getTotalRevenueForDate(weekStart.subtract(7,'d').format('YYYY-MM-DD'), weekEnd.subtract(7,'d').format('YYYY-MM-DD'));
+  
+      var products = await productService.getAll();
+      var services = await serviceService.getAll();
+  
+      var data = {
+        numbers: {
+          ongoingOrders: {
+              overall : overallOngoingOrders.length,
+              thisWeek : ongoingOrdersInCurrentWeek.length,
+          },
+          revenue : {
+            thisWeek : parseFloat( thisWeekRevenue[0]["sum(total)"] ),
+            lastWeel : parseFloat( lastWeekRevenue[0]["sum(total)"] )
+          },
+          productCount : products.length,
+          serviceCount : services.length
+        },
+      };
+  
+      response
+        .status(200)
+        .json(buildResponse({ ...data }))
+        .end();
+    } catch(error) {
+      response.status(500).json(buildResponse(null, error)).end()
+    }
+  });
+
+  app.get(DASHBOARD_BY_STORE, async (request, response) => {
+    try {
+      const storeId = request.params.storeId;
+      if ( isNaN(storeId) ) {
+          response.status(400).json(buildResponse(null, 'Invalid store id')).end()
+          return
+      }
+      var currentDate = moment();
+      var weekStart = currentDate.clone().startOf('week');
+      var weekEnd = currentDate.clone().endOf('week');
+  
+      var overallOngoingOrders = await orderService.getOverallOngoingOrdersByStore(storeId);
+      var ongoingOrdersInCurrentWeek =
+        await orderService.getOngoingOrdersForDatesAndStore(
+          weekStart.format('YYYY-MM-DD'),
+          weekEnd.format('YYYY-MM-DD'),
+          storeId
+        );
+  
+      var thisWeekRevenue = await orderService.getTotalRevenueForDateAndStore(weekStart.format('YYYY-MM-DD'), weekEnd.format('YYYY-MM-DD'), storeId);
+      var lastWeekRevenue = await orderService.getTotalRevenueForDateAndStore(weekStart.subtract(7,'d').format('YYYY-MM-DD'), weekEnd.subtract(7,'d').format('YYYY-MM-DD'), storeId);
   
       var products = await productService.getAll();
       var services = await serviceService.getAll();
