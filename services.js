f4acf56bb8d8ab35f6c414ba4c7bdb2fa15c7a53
biz-cloud-abhishek <103125284+biz-cloud-abhishek@users.noreply.github.com>
2022-11-29 19:30:26 -0600

    Manage services done

--- services.js
+++ services.js
@@ -2,14 +2,15 @@ import { buildResponse } from "./helper.functions.js";
 import serviceService from "./services/service.service.js";
 
 const BASE_URL = '/api';
-const GET_ALL_SERVICES = BASE_URL + '/service';
+const SERVICES = BASE_URL + '/service';
+const UPDATE_SERVICE = BASE_URL + '/updateService';
 const GET_SERVICES_BY_IDS = BASE_URL + '/serviceById';
 const GET_SERVICES_BY_CATEGORY = BASE_URL + '/serviceByCategory/:catgoryId';
 const GET_SOLD_SERVICE_COUNT = BASE_URL + '/serviceCount';
 const GET_SOLD_SERVICE_COUNT_BY_STORE = BASE_URL + '/serviceCount/:storeId';
 
 export default function (app) {
-    app.get(GET_ALL_SERVICES, async (request, response) => {
+    app.get(SERVICES, async (request, response) => {
         try {
             var services = await serviceService.getAll();
             response.status(200).json(buildResponse(services)).end()
@@ -64,6 +65,34 @@ export default function (app) {
             }
             var serviceCount = await serviceService.getServiceCountByStore(storeId);
             response.status(200).json(buildResponse(serviceCount)).end()
+        } catch(error) {
+            response.status(500).json(buildResponse(null, error)).end()
+        }
+    });
+
+    app.post(SERVICES, async (request, response) => {
+        try {
+            const service = request.body;
+            if ( service == null ) {
+                response.status(400).json(buildResponse(null, 'Invalid input')).end()
+                return
+            }
+            var id = await serviceService.createService(service);
+            response.status(200).json(buildResponse(id)).end();
+        } catch(error) {
+            response.status(500).json(buildResponse(null, error)).end()
+        }
+    });
+
+    app.post(UPDATE_SERVICE, async (request, response) => {
+        try {
+            const service = request.body;
+            if ( service == null ) {
+                response.status(400).json(buildResponse(null, 'Invalid input')).end()
+                return
+            }
+            await serviceService.updateService(service);
+            response.status(200).json(buildResponse(true)).end();
         } catch(error) {
             response.status(500).json(buildResponse(null, error)).end()
         }
