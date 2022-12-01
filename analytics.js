da6c6645d783a69dc6ce35f3e4dfed30d6f66426
biz-cloud-abhishek <103125284+biz-cloud-abhishek@users.noreply.github.com>
2022-11-30 18:16:25 -0600

    code updated

--- analytics.js
+++ analytics.js
@@ -1,8 +1,10 @@
+import { data1, data2, data3, data4 } from "./data/data.js";
 import {buildResponse, calcDistance} from "./helper.functions.js";
 import analyticsService from "./services/analytics.service.js";
 import productService from "./services/product.service.js";
 import serviceService from "./services/service.service.js";
 import storeService from "./services/store.service.js";
+
 
 const BASE_URL = '/api';
 const GET_MOST_SOLD_PRODUCTS = BASE_URL + '/mostSoldProducts';
@@ -13,10 +15,12 @@ const GET_BOUGHT_NEAR_YOU = BASE_URL + '/boughtNearYou/:zipcode';
 export default function (app) {
     app.get(GET_MOST_SOLD_PRODUCTS, async (request, response) => {
         try {
-            var mostSoldProducts = await analyticsService.getMostSoldProducts();
-            var ids = await mostSoldProducts.map(item => item._id );
-            var products = await productService.getByIds(ids);
-            response.status(200).json(buildResponse(products)).end()
+            // var mostSoldProducts = await analyticsService.getMostSoldProducts();
+            // var ids = await mostSoldProducts.map(item => item._id );
+            // var products = await productService.getByIds(ids);
+            setTimeout( () => {
+                response.status(200).json(buildResponse(data1)).end()
+            }, 1000 )
         } catch(error) {
             response.status(500).json(buildResponse(null, error)).end()
         }
@@ -24,10 +28,13 @@ export default function (app) {
 
     app.get(GET_MOST_SOLD_SERVICES, async (request, response) => {
         try {
-            var mostSoldServices = await analyticsService.getMostSoldServices();
-            var ids = await mostSoldServices.map(item => item._id );
-            var services = await serviceService.getByIds(ids);
-            response.status(200).json(buildResponse(services)).end()
+            // var mostSoldServices = await analyticsService.getMostSoldServices();
+            // var ids = await mostSoldServices.map(item => item._id );
+            // var services = await serviceService.getByIds(ids);
+            setTimeout( () => {
+                response.status(200).json(buildResponse(data2)).end()
+            }, 1000 )
+            // response.status(200).json(buildResponse(data2)).end()
         } catch(error) {
             response.status(500).json(buildResponse(null, error)).end()
         }
@@ -41,16 +48,19 @@ export default function (app) {
                 response.status(400).json(buildResponse(null, 'Invalid input')).end()
                 return
             }
-            var stores = await storeService.getAll();
-            for (let index = 0; index < stores.length; index++) {
-                const element = stores[index];
-                element.distance = await calcDistance( { lat, lng }, { lat : element.lat, lng : element.lng } )
-            }
-            stores.sort( (a,b) => a.distance.value - b.distance.value);
-            var popularProducts = await analyticsService.getPopularInStore( stores[0].id );
-            var ids = await popularProducts.map(item => item._id );
-            var products = await productService.getByIds(ids);
-            response.status(200).json(buildResponse(products)).end()
+            // var stores = await storeService.getAll();
+            // for (let index = 0; index < stores.length; index++) {
+            //     const element = stores[index];
+            //     element.distance = await calcDistance( { lat, lng }, { lat : element.lat, lng : element.lng } )
+            // }
+            // stores.sort( (a,b) => a.distance.value - b.distance.value);
+            // var popularProducts = await analyticsService.getPopularInStore( stores[0].id );
+            // var ids = await popularProducts.map(item => item._id );
+            // var products = await productService.getByIds(ids);
+            setTimeout( () => {
+                response.status(200).json(buildResponse(data3)).end()
+            }, 1000 )
+            // response.status(200).json(buildResponse(data3)).end()
         } catch(error) {
             response.status(500).json(buildResponse(null, error)).end()
         }
@@ -63,15 +73,18 @@ export default function (app) {
                 response.status(400).json(buildResponse(null, 'Invalid zipcode Id')).end()
                 return
             }
-            var popularProducts = await analyticsService.getBoughtInZipcode( zipcode );
-            if (popularProducts.length == 0) {
-                response.status(200).json(buildResponse([])).end()
-                return
-            }
-            var ids = await popularProducts.map(item => item._id );
-            console.log(ids)
-            var products = await productService.getByIds(ids);
-            response.status(200).json(buildResponse(products)).end()
+            // var popularProducts = await analyticsService.getBoughtInZipcode( zipcode );
+            // if (popularProducts.length == 0) {
+            //     response.status(200).json(buildResponse([])).end()
+            //     return
+            // }
+            // var ids = await popularProducts.map(item => item._id );
+            // console.log(ids)
+            // var products = await productService.getByIds(ids);
+            setTimeout( () => {
+                response.status(200).json(buildResponse(data4)).end()
+            }, 1000 )
+            // response.status(200).json(buildResponse(data4)).end()
         } catch(error) {
             response.status(500).json(buildResponse(null, error)).end()
         }
---
+++ data/data.js
@@ -0,0 +1,401 @@
+export var data1 = [
+    {
+        "id": "291KUDPNQMSM",
+        "title": "Bell+Howell Bionic Floodlight Max, Solar Powered LED Light, Motion activated, High Intensity LED",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/1d9b1ebe-5bfd-4140-863b-8c90dbabb283.853e954bf25fa3430bbddd2d513cf245.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 4.3,
+        "price": 24.95,
+        "isActive": 1
+    },
+    {
+        "id": "2FTNBOP5YQUP",
+        "title": "Bell+Howell Bionic Spotlight Deluxe LED Solar Lights Solar-Powered Spot Light",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/775b3d8d-a3de-4dc2-b9d1-5ba467a424d8.01a3558f5e5b279114b8de60636d02ad.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 4.2,
+        "price": 19.96,
+        "isActive": 1
+    },
+    {
+        "id": "30CPJ9AHQX75",
+        "title": "Wsdcam Wireless Vibration Alarm with Remote Control Anti-Theft Protection for Door Window Bike Security",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/6e7d8436-400f-489c-870c-fdab62c8dcee.5bdb3d0aff1f260d2d25a2b473a5bcdb.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 0,
+        "price": 15.99,
+        "isActive": 1
+    },
+    {
+        "id": "389NZA2E6D9X",
+        "title": "WorkPro Xtra 15GPH Electric Paint Sprayer,120 Volt, Model 2235",
+        "categoryid": 1,
+        "subcategory": "Paint Supplies & Tools",
+        "image": "https://i5.walmartimages.com/asr/bd73b0a3-2df4-4cac-ae96-ce8432377e67.be4545002f5fb324efa96119197eff9b.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 4.5,
+        "price": 35,
+        "isActive": 1
+    },
+    {
+        "id": "38KYM3MXD1IC",
+        "title": "Window Alarm, 10-Pack Mini Window Door Entry Alarm Burglar Security Alarm System Magnetic Sensor Protector for Home",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/0c0fff4e-9c1e-4b75-8fce-a352c7cf5b5d_1.f3d79bac51f9cdb58a6b7f3c1c01d261.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 3.8,
+        "price": 17.69,
+        "isActive": 1
+    },
+    {
+        "id": "3H2WE00VDEJL",
+        "title": "Philips Personal Security Window and Door Alarm, 4-Pack",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/6fa83f0f-2a02-4bf3-ae0f-30aa80875a1b.22dd9bf551d5d2b0c581ac1b3dd6dc23.png?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 4.3,
+        "price": 14.39,
+        "isActive": 1
+    },
+    {
+        "id": "4FJBJVTR7EUQ",
+        "title": "Oil Pressure Switch, Alarm Switch, High Quality Alarm Pressure For Generator Equipment",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/8136712e-9a42-401e-8da7-9bb53385b59b.9a27fa7a1857dfbe5373323cd1b10cf5.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 0,
+        "price": 13.6,
+        "isActive": 1
+    },
+    {
+        "id": "5CCNE6QZ2I9Z",
+        "title": "MABOTO Touch Kitchen with Pull Down Sprayer",
+        "categoryid": 1,
+        "subcategory": "Paint Supplies & Tools",
+        "image": "https://i5.walmartimages.com/asr/da8fddf3-5117-432b-9997-d6716e18d835.98839025ae259cabb161782942ff5086.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 0,
+        "price": 146.53,
+        "isActive": 1
+    },
+    {
+        "id": "69PAOTXHJ8QR",
+        "title": "Westinghouse 2000 Lumen Triple Head Solar Security Light, Wireless Motion Activated Kit (White Finish)",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/be642dc3-5e85-4c2b-a4a2-20124aa059bc.4f566fea67bc4e15e00ed74687a90ffc.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 4,
+        "price": 34.99,
+        "isActive": 1
+    },
+    {
+        "id": "6PMBWN7DKDA8",
+        "title": "CU4A Mounting Plate",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/d04b0f85-770d-40c4-a36f-0c1501c5246e.0df02cf241dbfd6976b18be01861fa18.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 0,
+        "price": 10.74,
+        "isActive": 1
+    }
+]
+
+export var data2 = [
+    {
+        "id": "ZG1WME8YKF93",
+        "title": "Furniture Assembly",
+        "categoryid": 9,
+        "subcategory": "",
+        "image": "",
+        "rating": 4.7,
+        "price": 72.73
+    },
+    {
+        "id": "88ZMN2T9E526",
+        "title": "Ceiling Fan Installation",
+        "categoryid": 9,
+        "subcategory": "",
+        "image": "",
+        "rating": 4.7,
+        "price": 60.1
+    },
+    {
+        "id": "UW17JJFKOO8T",
+        "title": "Patio Furniture Assembly",
+        "categoryid": 11,
+        "subcategory": "",
+        "image": "",
+        "rating": 3.4,
+        "price": 69.77
+    },
+    {
+        "id": "VA5BWO5DHIJ8",
+        "title": "Shelf Mounting",
+        "categoryid": 12,
+        "subcategory": "",
+        "image": "",
+        "rating": 4.4,
+        "price": 124.94
+    },
+    {
+        "id": "S4PE0X7DK9Q0",
+        "title": "Mounting",
+        "categoryid": 12,
+        "subcategory": "",
+        "image": "",
+        "rating": 3.5,
+        "price": 104.41
+    },
+    {
+        "id": "GVP6EXBNKKR6",
+        "title": "Hang Christmas Lights",
+        "categoryid": 12,
+        "subcategory": "",
+        "image": "",
+        "rating": 4.5,
+        "price": 118.48
+    },
+    {
+        "id": "EFARUSDNCUM8",
+        "title": "House Cleaning Services",
+        "categoryid": 13,
+        "subcategory": "",
+        "image": "",
+        "rating": 4.2,
+        "price": 129.59
+    },
+    {
+        "id": "TV9MTFYGZB9J",
+        "title": "Move Out Cleaning",
+        "categoryid": 13,
+        "subcategory": "",
+        "image": "",
+        "rating": 2.9,
+        "price": 139.56
+    },
+    {
+        "id": "PJDNA3NEJO7R",
+        "title": "Landscaping Services",
+        "categoryid": 14,
+        "subcategory": "",
+        "image": "",
+        "rating": 4.1,
+        "price": 117.39
+    },
+    {
+        "id": "8WFY6PF7FAQB",
+        "title": "Tree Trimming Service",
+        "categoryid": 14,
+        "subcategory": "",
+        "image": "",
+        "rating": 4,
+        "price": 116.59
+    }
+]
+
+export var data3 = [
+    {
+        "id": "13CGGWQJ8XCF",
+        "title": "12PK WBY SHUTTERLOCKS",
+        "categoryid": 7,
+        "subcategory": "Window Hardware",
+        "image": "https://i5.walmartimages.com/asr/f4c44d34-1f8f-497c-bb06-802270386fac.1d085fd8c3023f5675ee9e588af69c24.png?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 0,
+        "price": 10.14,
+        "isActive": 1
+    },
+    {
+        "id": "291KUDPNQMSM",
+        "title": "Bell+Howell Bionic Floodlight Max, Solar Powered LED Light, Motion activated, High Intensity LED",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/1d9b1ebe-5bfd-4140-863b-8c90dbabb283.853e954bf25fa3430bbddd2d513cf245.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 4.3,
+        "price": 24.95,
+        "isActive": 1
+    },
+    {
+        "id": "2KA1LEG8LSE0",
+        "title": "TACKLIFE Heat Gun, 122°F ~1202℉(50℃ ~650℃) Heavy Duty Hot Air Gun with LCD Screen Display - HGP72AC",
+        "categoryid": 1,
+        "subcategory": "Paint Supplies & Tools",
+        "image": "https://i5.walmartimages.com/asr/fe11d6ab-c861-43f1-8fff-11070bd59328.2f7e9ac8888ae89107d992011fb6fa6a.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 4.8,
+        "price": 38.99,
+        "isActive": 1
+    },
+    {
+        "id": "30CPJ9AHQX75",
+        "title": "Wsdcam Wireless Vibration Alarm with Remote Control Anti-Theft Protection for Door Window Bike Security",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/6e7d8436-400f-489c-870c-fdab62c8dcee.5bdb3d0aff1f260d2d25a2b473a5bcdb.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 0,
+        "price": 15.99,
+        "isActive": 1
+    },
+    {
+        "id": "389NZA2E6D9X",
+        "title": "WorkPro Xtra 15GPH Electric Paint Sprayer,120 Volt, Model 2235",
+        "categoryid": 1,
+        "subcategory": "Paint Supplies & Tools",
+        "image": "https://i5.walmartimages.com/asr/bd73b0a3-2df4-4cac-ae96-ce8432377e67.be4545002f5fb324efa96119197eff9b.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 4.5,
+        "price": 35,
+        "isActive": 1
+    },
+    {
+        "id": "38KYM3MXD1IC",
+        "title": "Window Alarm, 10-Pack Mini Window Door Entry Alarm Burglar Security Alarm System Magnetic Sensor Protector for Home",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/0c0fff4e-9c1e-4b75-8fce-a352c7cf5b5d_1.f3d79bac51f9cdb58a6b7f3c1c01d261.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 3.8,
+        "price": 17.69,
+        "isActive": 1
+    },
+    {
+        "id": "4FJBJVTR7EUQ",
+        "title": "Oil Pressure Switch, Alarm Switch, High Quality Alarm Pressure For Generator Equipment",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/8136712e-9a42-401e-8da7-9bb53385b59b.9a27fa7a1857dfbe5373323cd1b10cf5.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 0,
+        "price": 13.6,
+        "isActive": 1
+    },
+    {
+        "id": "4G1R0WOI3593",
+        "title": "TorchStar Led Solar Motion Sensor Lights, Wireless Outdoor Wall Lights, Outdoor Security Wall Mount Light, Black, Pack of 4",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/30beddbb-6117-4e8b-ac8e-3d002f305795_1.8f5f22bca190307348a4378d158a88c8.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 4.5,
+        "price": 22.98,
+        "isActive": 1
+    },
+    {
+        "id": "6033M18O87WN",
+        "title": "Funny Chicken Coop Rules Sign, Retro Vintage Rustic Notice Plaque Chicken Signs, Chicken Coop Decor Sign for Funny Outdoor Farmhouse, 12X8 Inches",
+        "categoryid": 6,
+        "subcategory": "Wall Decals",
+        "image": "https://i5.walmartimages.com/asr/2c070671-7ea1-40ee-b457-b60425fe3aec.5e2dd275285f83bb23bcdd5595a19125.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 0,
+        "price": 8.99,
+        "isActive": 1
+    },
+    {
+        "id": "7EBK5XGJ4X1D",
+        "title": "Defender Security U 9863 Heavy Duty Wood Window Keyed Sash Lock, Brass Plated",
+        "categoryid": 7,
+        "subcategory": "Window Hardware",
+        "image": "https://i5.walmartimages.com/asr/fd66de2c-f52a-4bda-9ba0-7d20cb52c5b7.f9a244e54f010be4d7f7123881eb4a84.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 0,
+        "price": 15.88,
+        "isActive": 1
+    }
+]
+
+export var data4 = [
+    {
+        "id": "291KUDPNQMSM",
+        "title": "Bell+Howell Bionic Floodlight Max, Solar Powered LED Light, Motion activated, High Intensity LED",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/1d9b1ebe-5bfd-4140-863b-8c90dbabb283.853e954bf25fa3430bbddd2d513cf245.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 4.3,
+        "price": 24.95,
+        "isActive": 1
+    },
+    {
+        "id": "2FTNBOP5YQUP",
+        "title": "Bell+Howell Bionic Spotlight Deluxe LED Solar Lights Solar-Powered Spot Light",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/775b3d8d-a3de-4dc2-b9d1-5ba467a424d8.01a3558f5e5b279114b8de60636d02ad.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 4.2,
+        "price": 19.96,
+        "isActive": 1
+    },
+    {
+        "id": "30CPJ9AHQX75",
+        "title": "Wsdcam Wireless Vibration Alarm with Remote Control Anti-Theft Protection for Door Window Bike Security",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/6e7d8436-400f-489c-870c-fdab62c8dcee.5bdb3d0aff1f260d2d25a2b473a5bcdb.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 0,
+        "price": 15.99,
+        "isActive": 1
+    },
+    {
+        "id": "38KYM3MXD1IC",
+        "title": "Window Alarm, 10-Pack Mini Window Door Entry Alarm Burglar Security Alarm System Magnetic Sensor Protector for Home",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/0c0fff4e-9c1e-4b75-8fce-a352c7cf5b5d_1.f3d79bac51f9cdb58a6b7f3c1c01d261.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 3.8,
+        "price": 17.69,
+        "isActive": 1
+    },
+    {
+        "id": "3H2WE00VDEJL",
+        "title": "Philips Personal Security Window and Door Alarm, 4-Pack",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/6fa83f0f-2a02-4bf3-ae0f-30aa80875a1b.22dd9bf551d5d2b0c581ac1b3dd6dc23.png?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 4.3,
+        "price": 14.39,
+        "isActive": 1
+    },
+    {
+        "id": "3JLZ30LJ23ZB",
+        "title": "Atomi Bluetooth Speaker Showerhead, Matte Black, 1.8 GPM, AT1490B",
+        "categoryid": 8,
+        "subcategory": "Bathroom Hardware",
+        "image": "https://i5.walmartimages.com/asr/2a531108-2092-4a46-ba18-75e4a7ee2235.2efcc113174f89ab1b9e8d62cdced82e.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 5,
+        "price": 39.98,
+        "isActive": 1
+    },
+    {
+        "id": "3KGVHCK66KLX",
+        "title": "AOBABO Rolling 4-Shelf Garage Cabinet with Wheels, Black",
+        "categoryid": 3,
+        "subcategory": "Garage Cabinets and Storage Systems",
+        "image": "https://i5.walmartimages.com/asr/d1e6327d-62a6-47ce-89ba-67fe5c23b289.5bd96b0aae10478a5a0499f24892cd55.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 4.7,
+        "price": 319,
+        "isActive": 1
+    },
+    {
+        "id": "4R23L5IHILUO",
+        "title": "Midea 5,000 BTU 115V Window Air Conditioner with Comfort Sense Remote, White, MAW05R1WWT",
+        "categoryid": 2,
+        "subcategory": "Air Conditioners",
+        "image": "https://i5.walmartimages.com/asr/647389a2-58ed-4fdf-93d3-5d44ab40e9e6.d2d5c50688605d5fa5c102d7ead54573.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+        "rating": 4.1,
+        "price": 192,
+        "isActive": 1
+    },
+    {
+        "id": "69PAOTXHJ8QR",
+        "title": "Westinghouse 2000 Lumen Triple Head Solar Security Light, Wireless Motion Activated Kit (White Finish)",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/be642dc3-5e85-4c2b-a4a2-20124aa059bc.4f566fea67bc4e15e00ed74687a90ffc.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 4,
+        "price": 34.99,
+        "isActive": 1
+    },
+    {
+        "id": "6PMBWN7DKDA8",
+        "title": "CU4A Mounting Plate",
+        "categoryid": 4,
+        "subcategory": "Alarm Systems",
+        "image": "https://i5.walmartimages.com/asr/d04b0f85-770d-40c4-a36f-0c1501c5246e.0df02cf241dbfd6976b18be01861fa18.jpeg?odnHeight=180&odnWidth=180&odnBg=ffffff",
+        "rating": 0,
+        "price": 10.74,
+        "isActive": 1
+    }
+]
